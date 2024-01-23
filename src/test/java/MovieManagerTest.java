a32a0b2a6e7f31c20276d619a2f05e309ddb48e6
MRuban <rubanmariyaaleksandrovna@gmail.com>
2024-01-23 15:30:34 +0300

    test add

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -53,7 +53,7 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void findLastLimited() {
+    public void findLastLimitedMin() {
         MovieManager manager_limited = new MovieManager(3);
 
         manager_limited.addMovie("1 Бладшот");
@@ -65,6 +65,51 @@ public class MovieManagerTest {
         manager_limited.addMovie("7 Номер один");
         String[] actual = manager_limited.findLast();
         String[] expected = {"7 Номер один", "6 Тролли. Мировой тур", "5 Человек-невидимка",};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastLimitedMax() {
+        MovieManager manager_limited = new MovieManager(8);
+
+        manager_limited.addMovie("1 Бладшот");
+        manager_limited.addMovie("2 Вперёд");
+        manager_limited.addMovie("3 Отель Белград");
+        manager_limited.addMovie("4 Джентельмены");
+        manager_limited.addMovie("5 Человек-невидимка");
+        manager_limited.addMovie("6 Тролли. Мировой тур");
+        manager_limited.addMovie("7 Номер один");
+        String[] actual = manager_limited.findLast();
+        String[] expected = {"7 Номер один", "6 Тролли. Мировой тур", "5 Человек-невидимка", "4 Джентельмены", "3 Отель Белград", "2 Вперёд", "1 Бладшот"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastLimitedNormal() {
+        MovieManager manager_limited = new MovieManager(7);
+
+        manager_limited.addMovie("1 Бладшот");
+        manager_limited.addMovie("2 Вперёд");
+        manager_limited.addMovie("3 Отель Белград");
+        manager_limited.addMovie("4 Джентельмены");
+        manager_limited.addMovie("5 Человек-невидимка");
+        manager_limited.addMovie("6 Тролли. Мировой тур");
+        manager_limited.addMovie("7 Номер один");
+        String[] actual = manager_limited.findLast();
+        String[] expected = {"7 Номер один", "6 Тролли. Мировой тур", "5 Человек-невидимка", "4 Джентельмены", "3 Отель Белград", "2 Вперёд", "1 Бладшот"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastMin() {
+        manager.addMovie("1 Бладшот");
+        manager.addMovie("2 Вперёд");
+        manager.addMovie("3 Отель Белград");
+        String[] actual = manager.findLast();
+        String[] expected = {"3 Отель Белград", "2 Вперёд", "1 Бладшот"};
 
         Assertions.assertArrayEquals(expected, actual);
     }
